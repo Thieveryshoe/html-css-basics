9b6dc5d595f47a02e35727218b8a6cfc5b480dc9
John Ellison <jellison@enterprisebank.com>
2021-05-13 14:29:33 -0500

    working with two widgets

--- Spikes/tableWithWidget/circleWidget.js
+++ Spikes/tableWithWidget/circleWidget.js
@@ -1,34 +1,45 @@
-let yb = { id: function (str) { return document.getElementById(str) } };
-let roe = .09;
-let roePercentage = (roe * 100).toFixed(2);
-setProgress(roePercentage, true);
+let existingWidget = { 
+    progressCircle: document.getElementById('existingProgress'),
+    sliderValue: document.getElementById('existingSliderValue')
+};
 
-function setProgress(roePercentage, showVal) {
-    const progressCircle = 'progress';
-    const sliderValue = 'sliderValue';
+let proposedWidget = { 
+    progressCircle: document.getElementById('proposedProgress'),
+    sliderValue: document.getElementById('proposedSliderValue')
+};
 
-    if (showVal == true) yb.id(sliderValue).innerHTML = roePercentage + '%';
+let existingRoe = toPercentage(.02);
+let proposedRoe = toPercentage(.09);
 
-    let radius = yb.id(progressCircle).getAttribute('r');
+setProgress(existingWidget, existingRoe, true);
+setProgress(proposedWidget, proposedRoe, true);
+
+function setProgress(widget, roe, showVal) {
+    if (showVal == true)
+        widget.sliderValue.innerHTML = roe + '%';
+
+    let radius = widget.progressCircle.getAttribute('r');
     let circumference = 2 * Math.PI * radius;
 
-
-    let progress_in_percent = roePercentage / 18 * 100;
+    let progress_in_percent = roe / 18 * 100;
     if (progress_in_percent > 100) progress_in_percent = 100;
     let progress_in_pixels = circumference * (100 - progress_in_percent) / 100;
-    yb.id(progressCircle).style.strokeDashoffset = progress_in_pixels + 'px';
+    widget.progressCircle.style.strokeDashoffset = progress_in_pixels + 'px';
 
-    
-    if (roePercentage < 9) {
-        yb.id(progressCircle).style.stroke = '#FA3232'; // red
-        yb.id(sliderValue).style.color = '#FA3232';
+    if (roe < 9) {
+        widget.progressCircle.style.stroke = '#FA3232'; // red
+        // widget.sliderValue.style.color = '#FA3232';
     }
-    else if (roePercentage >= 18) {
-        yb.id(progressCircle).style.stroke = '#329664'; // green
-        yb.id(sliderValue).style.color = '#329664';
+    else if (roe >= 18) {
+        widget.progressCircle.style.stroke = '#329664'; // green
+        // widget.sliderValue.style.color = '#329664';
     }
     else {
-        yb.id(progressCircle).style.stroke = '#FAC800'; // gold
-        yb.id(sliderValue).style.color = '#FAC800';
+        widget.progressCircle.style.stroke = '#FAC800'; // gold
+        // widget.sliderValue.style.color = '#FAC800';
     }
+}
+
+function toPercentage(roe) {
+    return (roe * 100).toFixed(2);
 }
